63602c193a778bb72031dde0d52dda21a3670fbd
Zach Miller <theunsocialunaware@tuta.io>
2026-04-03 17:36:47 -0400

    Working on battle class
    Battle will be used for fights within the game

--- systems/battle.py
+++ systems/battle.py
@@ -1,6 +1,7 @@
 import colors
 import config
-from utils import loading_json_entities
+import random
+from utils import loading_json_entities, talk
 
 loading_json_entities()
 
@@ -8,6 +9,7 @@ class battle:
     def __init__(self, preset: str):
         preset = preset.lower()
 
+        self.enemy_display_name = entities['enemies'][preset]['display_name']
         self.enemy_health = entities['enemies'][preset]['health']
         self.enemy_weapon = entities['enemies'][preset]['weapon']
         self.enemy_armor = entities['enemies'][preset]['armor']
@@ -16,3 +18,20 @@ class battle:
             self.drop = entities['enemies'][element].key()
             self.drop_chances = entities['enemies'][element].value()
         self.gold_drop = entities['enemies'][preset]['health'] * .4
+
+    def fight_start(self, custom_entry_phrase: str = ""):
+        entry_phrasels = [
+            f"{self.enemy_display_name} looks in your eyes with pure rage.",
+            f"{self.enemy_display_name} walks towards you, weapon in hand."
+        ]
+
+        if custom_entry_phrase:
+            entry_phrase = custom_entry_phrase
+        else:
+            entry_phrase = random.choice(entry_phrasels)
+
+        talk(entry_phrase)
+    
+    def 
+
+    def player_turn(self):

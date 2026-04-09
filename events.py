92f101b1bab9fd05a17a316582dc12e4cc0417e3
MasterMind4-0 <119766429+MasterMind4-0@users.noreply.github.com>
2026-04-08 23:37:42 -0400

    fix: skeleton_attack works now

--- events.py
+++ events.py
@@ -1,5 +1,6 @@
 import colors
 import config
+import random
 from systems.tavern import tavern
 from systems.battle import battle
 from utils import talk, format_name, wait, calculate_chance, player_gained
@@ -17,22 +18,21 @@ def visting_tavern():
     tavern(tavern_name).entering_tavern()
 
 def skeleton_attack():
-    global player_health, coin
     talk('You come across a dead body, its skeleton showing through the rotton flesh.')
     choice = input('Should you loot the body, walk away, or bury the body? (1, 2, or 3)\n')
-    if sfix(choice) == '1':
+    if choice == '1':
         player_gained(random.randint(6, 14))
         if calculate_chance(.6):
             talk('After you loot the body, it suddenly animates as you come near.')
             talk('It screeches and lunges at you.')
-            battle('Skeleton').fight()
+            battle('Skeleton', False).fight()
             talk('You wipe the sweat off your brow.')
             talk('You also loot the body and find a couple coins.')
         else:
             talk('You loot the body of its past life\'s belongings.')
-    elif sfix(choice) == '2':
+    elif choice == '2':
         talk('You decided to leave, best to leave the dead to the body collectors.')
-    elif sfix(choice) == '3':
+    elif choice == '3':
         if calculate_chance(.4):
             talk('The body animates as you come near,')
             talk('It screeches and lunges at you.')

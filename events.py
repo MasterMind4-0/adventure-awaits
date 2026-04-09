36901e2790b16d5f179b0a7e4f3840d048f31c65
MasterMind4-0 <119766429+MasterMind4-0@users.noreply.github.com>
2026-04-08 21:15:31 -0400

    event: skeleton fight added
    Uses the new util function for player gaining coin and items.

--- events.py
+++ events.py
@@ -1,7 +1,8 @@
 import colors
 import config
 from systems.tavern import tavern
-from utils import talk, format_name, wait
+from systems.battle import battle
+from utils import talk, format_name, wait, calculate_chance, player_gained
 
 def visting_tavern():
     tavern_name = [
@@ -14,7 +15,35 @@ def visting_tavern():
     ]
     
     tavern(tavern_name).entering_tavern()
+
+def skeleton_attack():
+    global player_health, coin
+    talk('You come across a dead body, its skeleton showing through the rotton flesh.')
+    choice = input('Should you loot the body, walk away, or bury the body? (1, 2, or 3)\n')
+    if sfix(choice) == '1':
+        player_gained(random.randint(6, 14))
+        if calculate_chance(.6):
+            talk('After you loot the body, it suddenly animates as you come near.')
+            talk('It screeches and lunges at you.')
+            battle('Skeleton').fight()
+            talk('You wipe the sweat off your brow.')
+            talk('You also loot the body and find a couple coins.')
+        else:
+            talk('You loot the body of its past life\'s belongings.')
+    elif sfix(choice) == '2':
+        talk('You decided to leave, best to leave the dead to the body collectors.')
+    elif sfix(choice) == '3':
+        if calculate_chance(.4):
+            talk('The body animates as you come near,')
+            talk('It screeches and lunges at you.')
+            battle('skeleton', False).fight()
+            talk('Once you kill the undead. You bury the body.')
+            talk('You wipe the sweat off your brow as you leave.')
+        else:
+            talk('You bury the body, say your prayers, and leave.')
+    return
     
 eventsls = [
-    visting_tavern
+    visting_tavern,
+    skeleton_attack
 ]

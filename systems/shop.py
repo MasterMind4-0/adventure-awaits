01f52542dcc1768f5c93b0c1bb72458eada2780b
MasterMind4-0 <119766429+MasterMind4-0@users.noreply.github.com>
2026-05-14 08:57:20 -0400

    feat: shop system finished

--- systems/shop.py
+++ systems/shop.py
@@ -2,21 +2,26 @@ import colors
 import time
 import config
 import random
-from utils import talk, format_name, display_inventory
+from utils import talk, format_name, display_inventory, player_var_change
 
 class shop:
     def __init__(self, pool_category: str, shop_name: str, trader_name: str = 'Trader', item_pool: list = None):
         self.shop_name = shop_name
         self.trader_name = trader_name
+        self.trader = format_name(self.trader_name)
 
-        self.pool_category = pool_category
+        match pool_category:
+            case 'enemies':
+                raise ValueError('Enemies are not a valid category')
+            case _:
+                self.pool_category = pool_category
+
         if item_pool:
             self.item_pool = item_pool
         else:
             self.item_pool = self.generate_random_item_pool()
     
     def shop_menu(self):
-        traded = False
         trader_intros = [
             'What can I do you for?',
             'What will it be today?',
@@ -24,26 +29,56 @@ class shop:
             "Don't bargin, I am not going to give in.",
             "I don't do sales."
         ]
-        trader = format_name(self.trader_name)
 
-        talk(f'{trader}: {random.choice(trader_intros)}')
-
+        talk(f'{self.trader}: {random.choice(trader_intros)}')
         while True:
             print(f'''
             ---~~~### {colors.TITLE}{self.shop_name}{colors.END} ###~~~---
+            
+            {self.get_shop_items()}
 
             Coins: {colors.GOLD}{config.coin}{colors.END}
 
             Inventory:
                 {display_inventory()}
+
+            L for leaving
             ''')
+            choice = input()
+            if choice.lower() == 'l':
+                break
+            self.purchased_item(int(choice) - 1)
+    
+    def purchased_item(self, choice):
+        item_dict_link = config.entities[self.pool_category][self.chosen_items[choice]]
+
+        talk(f'{self.trader}: Ah! That\'s a good one!')
+        talk(f'{self.trader}: It\'ll be {item_dict_link['price']} coins.')
+        if config.coin < item_dict_link['price']:
+            talk(f'{self.trader}: I\'d be happy to had it over, but it looks like your pockets aren\'t deep enough.')
+            # Implement trading option if you don't have enough money
+        else:
+            player_var_change(coins=-item_dict_link['price'])
+            player_var_change(items=item_dict_link['name'])
+
+            talk(f'{self.trader}: Pleasure doing business!')
+        return
 
     def generate_random_item_pool(self):
         viable_items = []
-        chosen_item_amount = random.randint(3, 6)
+        #chosen_item_amount = random.randint(3, 6)
+        chosen_item_amount = 2
 
         for item in config.entities[self.pool_category].keys():
             if config.entities[self.pool_category][item]['in_shop']:
                 viable_items.append(item)
         
-        return random.sample(viable_items, k=chosen_item_amount)
+        self.chosen_items = random.sample(viable_items, k=chosen_item_amount)
+
+    def get_shop_items(self):
+        printed_output = ""
+
+        for item in self.chosen_items:
+            printed_output += f'{self.chosen_items.index(item) + 1}. {config.entities[self.pool_category][item]['display_name']}\n\t    '
+
+        return printed_output

cd6ae226290298d33f57b1f00f8009f9c8c0dd3c
Zach Miller <119766429+MasterMind4-0@users.noreply.github.com>
2026-09-19 19:00:38 -0400

    fix(spelling): Traveling_trader "ites" -> "items"

--- event/events/traveling_trader.py
+++ event/events/traveling_trader.py
@@ -19,7 +19,7 @@ possible_item_pools = [
 def traveling_trader():
     trader = format_name('Traveling Merchant')
 
-    talk('A merchant, with their backpack filled to the brim with ites, comes towards you.')
+    talk('A merchant, with their backpack filled to the brim with items, comes towards you.')
     talk(f'{trader}: Well hello there! Care to view my stores?')
     talk(f'The merchant wiped sweat off his brow.')
     choice = player_mc(['Yes', 'No'], 'View the merchant\'s stock?', leave_option=False)
